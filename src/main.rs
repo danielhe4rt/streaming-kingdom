518263bf4c7ba9cf17be87f351eb7c21b0f54a60
danielhe4rt <danielhe4rt@gmail.com>
2026-01-31 13:23:11 -0300

    feat: init

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use hyprland::async_closure;
+use hyprland::event_listener::{AsyncEventListener, EventListener};
+use obws::Client;
+use std::sync::Arc;
+use std::time::Duration;
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> hyprland::Result<()> {
+    let client = Arc::new(
+        Client::connect("localhost", 4455, Some("123123"))
+            .await
+            .unwrap(),
+    );
+
+    // clone once to move into the closure
+    let client_for_events = Arc::clone(&client);
+
+    let mut event_listener = AsyncEventListener::new();
+
+    event_listener.add_active_window_changed_handler(async_closure! {
+        move |data| {
+            println!("{data:#?}")
+
+        }
+    });
+
+
+    event_listener.start_listener_async().await?;
+
+    Ok(())
+}
