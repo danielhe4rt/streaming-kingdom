5765b69c023b5f8cf8c798b2aaeaff11e6914abc
danielhe4rt <danielhe4rt@gmail.com>
2026-06-18 18:20:00 -0300

    chore(domain): drop unused emote_cdn_url re-export

--- src/domain/mod.rs
+++ src/domain/mod.rs
@@ -5,10 +5,7 @@ pub mod events;
 pub mod stats;
 
 pub use app_event::{AppEvent, AppEventEntry, EventGroup};
-pub use chat::{
-    ChatBadge, ChatMessage, ChatMessageDeleted, ChatSignal, EmoteSpan, MessageFragment,
-    emote_cdn_url,
-};
+pub use chat::{ChatBadge, ChatMessage, ChatMessageDeleted, ChatSignal, EmoteSpan, MessageFragment};
 pub use commands::FeatureCommand;
 pub use events::{StreamEvent, SubTier};
 pub use stats::StreamStats;
